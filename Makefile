bad2135d0f763f279db13a040dfa70a4dac8ee01
Danil Zagoskin <stolen@yandex-team.ru>
2015-02-26 17:42:53 +0300

    start shell with local cluster

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 PROJECT = minishard
+NID := 1
+SHELL_OPTS = -sname minishard$(NID) -setcookie minishard_demo
 include erlang.mk
--- src/minishard_demo.erl
+++ src/minishard_demo.erl
@@ -3,5 +3,11 @@
 -export([nodes/1]).
 -compile({no_auto_import, nodes}).
 
+% Generate fake node list by changing a number in local node name
 nodes(_) ->
-    [node()].
+    BinNode = atom_to_binary(node(), latin1),
+    [make_node(BinNode, N) || N <- lists:seq(1,5)].
+
+make_node(BinPattern, N) ->
+    IOLNode = re:replace(BinPattern, "[0-9]+@", [integer_to_list(N), "@"]),
+    binary_to_atom(iolist_to_binary(IOLNode), latin1).
