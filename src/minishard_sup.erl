552f4171a2c831dbffeaac75a235ee007dd42b6e
Danil Zagoskin <stolen@yandex-team.ru>
2015-07-02 21:50:47 +0300

    remove old stuff: pinger and watcher

--- src/minishard_sup.erl
+++ src/minishard_sup.erl
@@ -2,7 +2,7 @@
 -behaviour(supervisor).
 
 -export([start_link/1]).
--export([join_cluster/2, get_pid/2, add_pinger/3, add_pinger/4, pingers/1]).
+-export([join_cluster/2, get_pid/2]).
 -export([init/1]).
 
 sup_name(root) ->
@@ -17,10 +17,6 @@ sup_name({pinger_guard, ClusterName, Node, _}) ->
 % Helper: get pid of started infrastructure part
 get_pid(undefined, _) ->
     throw(undefined_cluster);
-get_pid(ClusterName, pingers) when is_atom(ClusterName) ->
-    strict_whereis(sup_name({pingers, ClusterName}));
-get_pid(ClusterName, watcher) when is_atom(ClusterName) ->
-    strict_whereis(minishard_watcher:name(ClusterName));
 get_pid(ClusterName, shard) when is_atom(ClusterName) ->
     strict_whereis(minishard_shard:name(ClusterName));
 get_pid(ClusterName, PartName) when is_atom(ClusterName), is_atom(PartName) ->
@@ -46,42 +42,6 @@ join_cluster(ClusterName, CallbackMod) when is_atom(ClusterName), is_atom(Callba
     supervisor:start_child(sup_name(root), ClusterSpec).
 
 
-%% Insert a new pinger into the pingers sup
-add_pinger(ClusterName, Node, Watcher) when is_atom(ClusterName), is_atom(Node), is_pid(Watcher) ->
-    PingersSup = get_pid(ClusterName, pingers),
-    add_pinger(PingersSup, ClusterName, Node, Watcher).
-
-add_pinger(PingersSup, ClusterName, Node, Watcher) when is_pid(PingersSup), is_atom(Node), is_pid(Watcher) ->
-    PingerSupSpec = {Node,
-                     {?MODULE, start_link, [{pinger_guard, ClusterName, Node, Watcher}]},
-                     permanent, 150, supervisor, []},
-    case supervisor:start_child(PingersSup, PingerSupSpec) of
-        {ok, Pid} ->
-            {ok, Pid};
-        {error,{already_started,Pid}} ->
-            {ok, Pid};
-        Error ->
-            Error
-    end.
-
-
-pingers(ClusterName) when is_atom(ClusterName) ->
-    pingers(get_pid(ClusterName, pingers));
-pingers(PingersSup) when is_pid(PingersSup) ->
-    erlang:is_process_alive(PingersSup) orelse throw(dead_cluster),
-    Children = supervisor:which_children(PingersSup),
-    ValidPingers = [{Node, extract_pinger(PingersSup, Node, GuardPid)} || {Node, GuardPid, _, _} <- Children],
-    lists:filter(fun({_, Pid}) -> is_pid(Pid) end, ValidPingers).
-
-extract_pinger(PingersSup, Node, GuardPid) ->
-    case supervisor:which_children(GuardPid) of
-        [{_, Pid, _, _}] ->
-            Pid;
-        _ ->
-            supervisor:terminate_child(PingersSup, Node),
-            supervisor:delete_child(PingersSup, Node),
-            undefined
-    end.
 
 init(root) ->
     GFixerSpec = {global_fixer,
@@ -94,23 +54,8 @@ init({cluster, ClusterName, CallbackMod}) ->
     AllocatorSpec = {allocator,
                      {minishard_allocator, start_link, [ClusterName, CallbackMod]},
                      permanent, 1000, worker, [minishard_allocator]},
-%    WatcherSpec = {watcher,
-%                   {minishard_watcher, start_link, [ClusterName, CallbackMod]},
-%                   permanent, 1000, worker, [minishard_watcher]},
-%    PingersSpec = {pingers,
-%                   {?MODULE, start_link, [{pingers, ClusterName}]},
-%                   permanent, 1000, supervisor, []},
     ShardSpec = {shard,
                  {minishard_shard, start_link, [ClusterName, CallbackMod]},
                    permanent, 1000, worker, [minishard_shard]},
 
-    {ok, {{one_for_all, 5, 10}, [AllocatorSpec, ShardSpec]}};
-
-init({pingers, _}) ->
-    {ok, {{one_for_one, 5, 5}, []}};
-
-init({pinger_guard, ClusterName, Node, Watcher}) ->
-    PingerSpec = {Node,
-                  {minishard_pinger, start_link, [ClusterName, Node, Watcher]},
-                  permanent, 100, worker, [minishard_pinger]},
-    {ok, {{one_for_all, 5, 1}, [PingerSpec]}}.
+    {ok, {{one_for_all, 5, 10}, [AllocatorSpec, ShardSpec]}}.
